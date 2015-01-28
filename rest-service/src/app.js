c169ec244ed8b87eeea67fd56d96e366c9397f5e
AdamThePieRat <adamjc86@gmail.com>
2015-01-28 21:09:09 +0000

    ok, should work this time...

---
+++ rest-service/src/app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var Roman = require('./roman-numerals.js');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});;
+
+app.get('/generate/:numeral', function (req, res) {
+    var romanNumeral = Roman.generate(parseInt(req.params.numeral));
+
+    res.send(romanNumeral);
+});
+
+app.get('/parse/:numeral', function (req, res) {
+    var arabicNumeral = Roman.parse(req.params.numeral);
+
+    res.send(arabicNumeral.toString());
+});
+
+app.listen(1337);
+console.log('Server active!');
