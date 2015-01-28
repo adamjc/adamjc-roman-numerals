55cd51c4cf4519f2d02dc2e3b8da0ca519172d48
Adam <adamjc86@gmail.com>
2015-01-28 21:02:28 +0000

    Update app.js

--- rest-service/src/app.js
+++ rest-service/src/app.js
@@ -3,6 +3,8 @@ var Roman = require('./roman-numerals.js');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET');
